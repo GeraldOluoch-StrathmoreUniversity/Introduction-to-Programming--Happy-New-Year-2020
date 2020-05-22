c96f4684050a1176dd2303c401fdbfe8f9140274
Gerald Oluoch <gerald.oluoch@strathmore.edu>
2020-05-22 17:11:23 +0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Happy_New_Year_2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Happy_New_Year_2020.dir/main.cpp.o"
+  "Happy_New_Year_2020"
+  "Happy_New_Year_2020.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Happy_New_Year_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
